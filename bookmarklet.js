b406d8a5845d73160be563e408d13484d53a93f3
kafene <kafene@users.noreply.github.com>
2014-08-08 21:13:56 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,19 +1,20 @@
-javascript:(function(u,f){var s=document.createElement('script');
-s.async=false;s.src=u;s.type='text/javascript';s.onload=function(){
-s.parentNode.removeChild(s);f()};document.head.appendChild(s);
-})('https://cdn.rawgit.com/kafene/grokfaster/master/grokfaster.js', function () {
-    var selectedText = String((function () {
-        if(window.getSelection){return window.getSelection()}
-        if(document.getSelection){return document.getSelection()}
-        if(document.selection){return document.selection.createRange().text}
-        return ''})());
-    window._grokfaster.options = {
+javascript:(function (url, cb) {
+    if (!window.grokfaster) {
+        var s = document.createElement('script'), h = document.head;
+        s.src = url;
+        s.onload = function () {h.removeChild(s); cb(window.grokfaster)};
+        h.appendChild(s);
+    } else {
+        cb(window.grokfaster);
+    }
+})('//cdn.rawgit.com/kafene/grokfaster/master/grokfaster.js', function (grokfaster) {
+    grokfaster.options = {
         wpm: 400,
         focal_point: true,
         dim_background: true,
         pause_sentence_time: 100,
         pause_other_time: 50,
-        show_additional: true
+        show_additional: true,
     };
-    window._grokfaster.grok(selectedText);
+    grokfaster.grok(getSelection().toString());
 });
